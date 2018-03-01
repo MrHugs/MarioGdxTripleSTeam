42918496c9012da968b7edccdbe8e110ff70daf7
MrHugs <hsanchezs01ex@gmail.com>
2018-03-01 09:38:17 +0100

    Primer push
    Creacion del proyecto MarioBrosss

---
+++ MarioBroSSS/desktop/src/com/mariobrosss/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mariobrosss.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mariobrosss.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
