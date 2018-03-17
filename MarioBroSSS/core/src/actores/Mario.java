a457280431115fb7e02514fa5f851e0b2410c9cc
MrHugs <hsanchezs01ex@gmail.com>
2018-03-17 20:09:02 +0100

    Animaciones empezadas, nada util por el mometo

--- MarioBroSSS/core/src/actores/Mario.java
+++ MarioBroSSS/core/src/actores/Mario.java
@@ -2,18 +2,20 @@ package actores;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.mariobrosss.game.Movimiento;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mariobrosss.game.Movimiento;
 
 import utiles.CollisionBits;
 import utiles.Constantes;
@@ -22,6 +24,7 @@ import utiles.MetricVector2;
 
 public class Mario extends MyActor {
 	final byte MAX_VIDA = 2;
+	private float elapsedTime=0;
 	byte vida = 1;
 	public Body body;
 	FixtureDef fixtura;
@@ -32,10 +35,20 @@ public class Mario extends MyActor {
 	public int isJumping = 0;
 	public boolean izquierdeando, derecheando = false;
 	Movimiento movimiento;
+	private TextureAtlas atlas;
+	private TextureRegion regionMario;
+	Animation animation;
 
 	public Mario(MetricVector2 position, World world, MetricSize size) {
 		super();
+		textureRegion = new TextureRegion();
+		atlas = new TextureAtlas(Gdx.files.internal("Mario_and_Enemies.pack"));
+		regionMario = new TextureRegion(atlas.findRegion("little_mario"),0,0,16,16);
+		setBounds(0, 0, 16/utiles.Constantes.PIXELS_TO_METERS,  16/utiles.Constantes.PIXELS_TO_METERS);
+		textureRegion.setRegion(regionMario);
 		this.size = size;
+		animation=new Animation(1/15f, atlas.findRegion("little_mario"));
+
 		BodyDef bodydef = new BodyDef();
 		bodydef.type = BodyType.DynamicBody;
 		bodydef.position.set(position.getMetersX(), position.getMetersY());
@@ -51,13 +64,15 @@ public class Mario extends MyActor {
 		body.createFixture(fixture);
 		body.setUserData("mario");
 		body.setFixedRotation(true);
-		sprite = defineSprite();
+		//sprite = defineSprite();
 		shape.dispose();
 	}
 
 	public void setMovimiento(Movimiento movimiento) {
 		this.movimiento = movimiento;
 	}
+
+	
 
 	private Sprite defineSprite() {
 		Sprite sprite = new Sprite(new Texture(Gdx.files.internal("mariobros.png")));
@@ -75,7 +90,10 @@ public class Mario extends MyActor {
 
 	public void draw(Batch batch) {
 		super.draw(batch, 1);
-		sprite.draw(batch);
+		//sprite.draw(batch);
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		batch.draw((TextureRegion)animation.getKeyFrame(elapsedTime, true), 0, 0);
+
 
 	}
 
@@ -84,7 +102,7 @@ public class Mario extends MyActor {
 		super.act(delta);
 		// System.out.println(derecheando +" DERECHEANDO");
 		// System.out.println(izquierdeando+ " IZQUIERDEANDO");
-		updateSpritePosition();
+	//	updateSpritePosition();
 		if (!derecheando && !izquierdeando || (izquierdeando && derecheando) || movimiento.bordeIzq()) {
 			body.setLinearVelocity(0f, body.getLinearVelocity().y);
 			if (movimiento.bordeIzq()) {
@@ -105,13 +123,13 @@ public class Mario extends MyActor {
 		}
 	}
 
-	private void updateSpritePosition() {
-		float x, y;
-		x = body.getPosition().x * Constantes.PIXELS_TO_METERS - sprite.getWidth() / 2;
-		y = body.getPosition().y * Constantes.PIXELS_TO_METERS - sprite.getHeight() / 2;
-		sprite.setPosition(x, y);
-		sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
-	}
+//	private void updateSpritePosition() {
+//		float x, y;
+//		x = body.getPosition().x * Constantes.PIXELS_TO_METERS - sprite.getWidth() / 2;
+//		y = body.getPosition().y * Constantes.PIXELS_TO_METERS - sprite.getHeight() / 2;
+//		sprite.setPosition(x, y);
+//		sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
+//	}
 
 	@Override
 	public boolean isColisionable() {
