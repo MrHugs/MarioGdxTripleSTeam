5bed8e1f4230d258fe7d34f5cb7aa98a2244fcf1
Raulglue <raul14-jl@hotmail.com>
2018-03-18 17:41:12 +0100

    Rebase a lo bestia

--- MarioBroSSS/core/src/com/mariobrosss/game/MyGame.java
+++ MarioBroSSS/core/src/com/mariobrosss/game/MyGame.java
@@ -1,5 +1,9 @@
 package com.mariobrosss.game;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TimerTask;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
@@ -17,24 +21,27 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-
 import actores.Bala;
+import actores.Cannon;
+import actores.CreadorBalas;
+import actores.Goomba;
 import actores.Mario;
 import actores.Suelo;
 import utiles.Constantes;
 import utiles.MetricSize;
 import utiles.MetricVector2;
 
-public class MyGame  {
+public class MyGame {
 
 	private static final float FactorZoomCamera = 1;
-	
+
 	private TmxMapLoader maploader;
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer renderer;
-	
+
 	World world;
 	Stage stage;
 	SpriteBatch batch;
@@ -46,34 +53,33 @@ public class MyGame  {
 	Suelo suelo, suelo2, suelo3;
 	Mario mario;
 	Bala bala;
+	Goomba goomba;
 	InputAdapter prueba;
 	Music music;
 	InputMultiplexer multiplexor;
-<<<<<<< HEAD
-	Cannon cannonUno;
-=======
-	
->>>>>>> 65945c2a03ba77e201c9e2a97c5ed8b1ca411f3d
+	Cannon cannonUno, cannonDos;
 	boolean pausa = false;
-
+	Timer timer;
+	ArrayList<Bala> listaBalas = new ArrayList<Bala>();
+	CreadorBalas creadorBalas;
 	public MyGame() {
 		super();
-		
+		timer = new Timer();
 		world = new World(new Vector2(Constantes.GRAVEDAD_X, Constantes.GRAVEDAD_Y), true);
 		maploader = new TmxMapLoader();
-		map  = maploader.load("level1.tmx");
+		map = maploader.load("level1.tmx");
 		renderer = new OrthogonalTiledMapRenderer(map);
 		new B2WorldCreator(world, map);
-		
+
 		music = Gdx.audio.newMusic(Gdx.files.internal("marioTheme.mp3"));
 		batch = new SpriteBatch();
 		debugRenderer = new Box2DDebugRenderer();
 		stage = new Stage();
+		// mario = new Mario(new MetricVector2(100, 20), world, new MetricSize(10, 16));
 		mario = new Mario(new MetricVector2(100, 100), world, new MetricSize(10, 16));
+		cannonUno = new Cannon(new MetricVector2(34 * Constantes.PIXELS_TO_METERS, 0.56f * Constantes.PIXELS_TO_METERS),
+				world, new MetricSize(20, 40));
 		camera = new GameCamera();
-		
-		
-		
 		movimiento = new Movimiento(mario);
 		// suelo = new Suelo(new MetricVector2(1692f, -256f), new MetricSize(2048, 64),
 		// world);
@@ -87,7 +93,7 @@ public class MyGame  {
 		bala = new Bala(new MetricVector2(34 * Constantes.PIXELS_TO_METERS, 0.56f * Constantes.PIXELS_TO_METERS), world,
 				new MetricSize(20, 15));
 		listaBalas.add(bala);
-//		creadorBalas = new CreadorBalas();
+		creadorBalas = new CreadorBalas();
 //        timer.schedule(new TimerTask() {
 //
 //            @Override
@@ -96,6 +102,7 @@ public class MyGame  {
 //            }
 //        }, 0, 2000);
 		stage.addActor(bala);
+		stage.addActor(cannonUno);
 		stage.addActor(mario);
 		multiplexor = new InputMultiplexer();
 		multiplexor.addProcessor(0, camera);
@@ -109,8 +116,20 @@ public class MyGame  {
 		music.setLooping(true);
 		music.setVolume(0.9f);
 		mario.setMovimiento(movimiento);
-		//camera.camera.position.set(stage.getViewport().getScreenWidth()/2, stage.getViewport().getWorldHeight()/4.2f,camera.camera.position.z);
+		// camera.camera.position.set(stage.getViewport().getScreenWidth()/2,
+		// stage.getViewport().getWorldHeight()/4.2f,camera.camera.position.z);
 
+	}
+
+	public void createBullet() {
+		Bala nueva;
+		try {
+			nueva = (Bala) bala.clone();
+			listaBalas.add(nueva);
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void act() {
@@ -125,7 +144,7 @@ public class MyGame  {
 
 	public void render() {
 		this.act();
-//		stage.act();
+		stage.act();
 		renderer.setView(camera.camera);
 		camera.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.update();
@@ -133,12 +152,24 @@ public class MyGame  {
 		debugMatrix = batch.getProjectionMatrix().cpy().scale(Constantes.PIXELS_TO_METERS, Constantes.PIXELS_TO_METERS,
 				0);
 		renderer.render();
+		for (Iterator iterator = listaBalas.iterator(); iterator.hasNext();) {
+			Bala bala = (Bala) iterator.next();
+			bala.update();
+			if (bala.isDead())
+				iterator.remove();
+		}
+		createBullet();
 		batch.begin();
 		mario.draw(batch);
-//		suelo.draw(batch);
-//		suelo2.draw(batch);
-//		suelo3.draw(batch);
-//		bala.draw(batch);
+		goomba.draw(batch);
+		cannonUno.draw(batch);
+		// suelo.draw(batch);
+		// suelo2.draw(batch);
+		// suelo3.draw(batch);
+		for (Bala bala : listaBalas) {
+			if (!bala.isDead())
+				bala.draw(batch);
+		}
 		batch.end();
 		debugRenderer.render(world, debugMatrix);
 
@@ -151,5 +182,4 @@ public class MyGame  {
 		stage.dispose();
 	}
 
-	
 }
